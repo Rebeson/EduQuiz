11471ef91f86c39abf09cbe87e4eb080b7c96b20
Rebeson <rebesonvitalino@gmail.com>
2025-12-18 22:42:51 -0300

    Adicionado as classes principais
    Aqui foi adicionado as primeiras classes do sistema.

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
 # Este projeto tem como objetivo avaliativo. Cuja funcionalidade principal é a criação de gerenciador de questões.
-import pandas as pd
+import sqlite3
+import json
+import os
+from enum import Enum
+
+
+class Dificuldade (Enum):
+    FACIL = 1
+    MEDIO = 2
+    DIFICIL = 3
+
+CONFIGURAÇÃO_PADRAO = {
+    "duração_padrão": 10,
+    "máximo_tentativas": 3,
+    "peso_dificuldade": {"FACIL": 1, "MEDIO": 2, "DIFICIL": 3}
+}
+
+
+class Pergunta:
+    def __init__(self, enunciado, alternativas, tema, dificuldade, resposta_certa):
+        if not (3 <= len(alternativas) <= 5):
+            raise ValueError("A pergunta deve ter entre 3 e 5 alternativas.")
+        if not (0 <= resposta_certa < len(alternativas)):
+            raise ValueError("Índice de resposta correta inválido.")
+        
+        self.enunciado = enunciado
+        self.alternativas = alternativas
+        self.resposta_certa = resposta_certa
+        self.dificuldade = dificuldade.upper()
+        self.tema = tema.upper()
+
+
+class BancoDados:
+    def __init__(self, nome_banco = "quiz_banco.db"):
+        self.conn = sqlite3.connect(nome_banco)
+        self._criar_tabela()
+
+
+class Sistema_Quiz:
+    def __init__(self):
+        pass
