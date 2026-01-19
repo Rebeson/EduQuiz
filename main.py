7f1aace2a464de0d29a247e262c519a8321d6290
Ramon Batista <50596238+ramon0306@users.noreply.github.com>
2026-01-18 21:11:25 -0300

    Banco de Dados

--- main.py
+++ main.py
@@ -68,6 +68,40 @@ def menu_principal():
             break
         else:
             print("Opção inválida!")
+ #BancoDeDados           
+def main():
+    bd = BancoDados()
+    config = Configuracao()
+    
+    while True:
+        print("\n=== BEM-VINDO AO EDUQUIZ UFCA ===")
+        print("1 - Fazer Login")
+        print("2 - Criar Nova Conta")
+        print("3 - Sair")
+        op = input("Opção: ").strip()
+        
+        if op == "1":
+            mat = input("Matrícula: ").strip()
+            sen = input("Senha: ").strip()
+            u = bd.autenticar(mat, sen)
+            if u: menu_restrito(bd, config, u)
+            else: print("\n[!] Matrícula ou senha incorretos.")
+            
+        elif op == "2":
+            print("\n--- FORMULÁRIO DE CADASTRO ---")
+            nome = input("Nome: ").strip()
+            mail = input("Email: ").strip()
+            mat = input("Matrícula: ").strip()
+            sen = input("Senha: ").strip()
+            tipo = input("É Administrador? (s/n): ").lower() == 's'
+            
+            novo_u = Usuario(nome, mail, mat, sen, Perfil.ADMIN if tipo else Perfil.ALUNO)
+            if bd.cadastrar_usuario(novo_u):
+                print("\n[OK] Conta criada! Você já pode fazer login.")
+            else:
+                print("\n[!] Erro: Esta matrícula já existe no sistema.")
+                
+        elif op == "3": sys.exit()
 
 if __name__ == "__main__":
     menu_principal()
